05456e0a8395a8eba4983f288843777d4056adf9
Victor Samayoa <vhs1092@gmail.com>
2019-09-17 11:51:53 -0600

    persist state data when read and dismiss post

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
 Vue.use(Vuex);
+import createPersistedState from 'vuex-persistedstate'
 
 const store = new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     count: 0,
     postList: [],
@@ -12,10 +13,14 @@ const store = new Vuex.Store({
 
   actions: {
     loadPosts({ commit }) {
+
+      if (this.state.postList.length == 0) {
         const api = 'https://www.reddit.com/r/all/top.json?limit=50';
+
         Vue.axios.get(api).then((response) => {
           commit('setPosts', response.data.data.children)
         });
+      }
     }
   },
 
